0d8452c0518274df307b8b0472ddca926fc571db
kx0101 <liakos.koulaxis@yahoo.com>
2024-12-04 00:20:11 +0200

    chore: refactored parsing of let and return statements. Changed RLPL to RPPL

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/kx0101/monkey-language/lexer"
-	"github.com/kx0101/monkey-language/token"
+	"github.com/kx0101/monkey-language/parser"
 )
 
 const PROMPT = ">> "
@@ -25,9 +25,25 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Error\n")
+	io.WriteString(out, " parser errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
